e4c2a1bc958caca2414afcb3a0de1a9459f411d6
shi-chika <hygge34living@gmail.com>
2024-03-10 01:47:52 +0900

    commit02

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
+
 function order() {
-    let getName = document.getElementById("input_name").value;
+  let getName = document.getElementById("input_name").value;
+  let typeData = document.getElementById("type");
+  let typeIndex = typeData.selectedIndex;
+  let getType = typeData.options[typeIndex].value;
   
+  let itemData = document.getElementById('item');
+  let itemIndex = itemData.selectedIndex;
+  let getItem = itemData.options[itemIndex].value;
   
-    let typeData = document.getElementById("type");
-    let typeIndex = typeData.selectedIndex;
-    let getType = typeData.options[typeIndex].value;
-  
-    let itemData = document.getElementById('item');
-    let itemIndex = itemData.selectedIndex;
-    let getItem = itemData.options[itemIndex].value;
-  
-    let getCount = document.getElementById('input_count').value;
-    let num_getCount = parseInt(getCount);
-  
+  let getCount = document.getElementById('input_count').value;
+  let num_getCount = parseInt(getCount);
+
     if (getName !== "") {
       if (getType === "none") {
-        alert('カテゴリを選択してください。');
+        alert('カテゴリを選択してください');
+
       } else if (getItem === "none") {
-        alert('アイテムを選択してください。');
+        alert('アイテムを選択してください');
+
       } else if (getCount === "") {
-        alert('数量を入力してください。');
+        alert('数量を入力してください');
+
       } else if (num_getCount < 3) {
-        alert('申し訳ありませんが、最低購入数量は3です。')
+        alert('申し訳ありませんが最少注文数は3点です')
+
       } else if (num_getCount > 20) {
-        alert('申し訳ありませんが、最高購入数量は20です。')
+        alert('申し訳ありませんが最高注文数は20点です')
+
       } else {
-        alert(getName +'様'+ '\nご注文いただき、ありがとうございます。\n下記、表示される購入明細に間違いないかご確認ください。')
+        alert(getName +'様'+ '\nご利用いただきありがとうございます\n下記注文内容をご確認いただけます')
+        
         document.getElementById("orderName").textContent = getName;
-  
         document.getElementById("orderType").textContent = getType;
         document.getElementById('orderItem').textContent = getItem;
         document.getElementById('orderCount').textContent = getCount;
@@ -35,26 +39,22 @@ function order() {
         if (getType === "メンズ") {
           let price = parseInt(getCount) * 1899
           document.getElementById('orderPrice').textContent = price + "円";
-          alert("今だけお得！！今日注文いただければ、メンズのネクタイをプレゼントいたします！");
+          alert("今だけお得！！今日メンズアイテムご購入の方にネクタイをプレゼントいたします！");
         } else if (getType === "レディース") {
           let price = parseInt(getCount) * 1499
           document.getElementById('orderPrice').textContent = price + "円";
-          alert("今だけお得！！今日注文いただければ、レディースのスカーフをプレゼントいたします！");
+          alert("今だけお得！！今日レディースアイテムご購入の方にスカーフをプレゼントいたします！");
         }
       }
   
     } else {
-      alert('お名前を入力してください。')
+      alert('お名前を入力してください。');
+      
     }
-  
-  
   
   }
   
   
-  
-
-
 function showMsg (){
   const msg = document.querySelector('.model');
   msg.style.display = 'block';
